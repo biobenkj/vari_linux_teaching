f04ea277e079625bf73714427ee010053b1e2e17
Ben Johnson <ben.johnson4343@gmail.com>
2017-03-29 20:32:21 -0400

    Update conf.py for aesthetic

--- source/conf.py
+++ source/conf.py
@@ -45,7 +45,7 @@ source_suffix = '.rst'
 master_doc = 'index'
 
 # General information about the project.
-project = u'vari_linux'
+project = u'VARI Linux Workshop'
 copyright = u'2017, Benjamin K Johnson'
 author = u'Benjamin K Johnson'
 
@@ -82,7 +82,7 @@ todo_include_todos = False
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'default'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
